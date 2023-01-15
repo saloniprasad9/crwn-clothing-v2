0db031413e4f50276b8a0b83d0c77b7e98d28040
saloniprasad9 <saloniprasad9@gmail.com>
2023-01-15 18:58:59 +0530

    ok

--- crwn-clothing/src/routes/sign-in/sign-in.component.jsx
+++ crwn-clothing/src/routes/sign-in/sign-in.component.jsx
@@ -1,17 +1,34 @@
-import { signInWithGooglePopup , createUserDocumentFromAuth  } from '../../utils/firebase/firebase.utils';
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+import { auth, signInWithGooglePopup , createUserDocumentFromAuth , signInWithGoogleRedirect  } from '../../utils/firebase/firebase.utils';
 
 const SignIn = () => {
+    useEffect( () => {
+        const response =  getRedirectResult(auth);
+        console.log(response);
+
+        if(response){
+            const userDocRef =  createUserDocumentFromAuth(response.user);
+        }
+
+    },[]);
     const logGoogleUser = async () => {
         const {user} = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user);
     }
+
+    
+
     return (
         <div>
             <h1>
                 Sign In Page
             </h1>
             <button onClick={logGoogleUser}>
-                Sign In With Google Popup
+                Sign In With Google Popup 
+            </button>
+            <button onClick={signInWithGoogleRedirect}>
+                Sign In With Google Redirect 
             </button>
         </div>
     );
